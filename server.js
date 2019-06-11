2dba68a69796748219a0d70cdcb378dee4f0d060
darkacer <omkardeokar95@gmail.com>
2019-06-11 08:23:29 +0530

    updates server.js

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+const express = require("express");
 var app = express();
-app.use(bodyParser.json());
+
 
 // Create link to Angular build directory
-var distDir = __dirname + "/dist/";
-app.use(express.static(distDir));
-
- var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-  });
+//var distDir = __dirname + "/dist/";
+app.use(express.static(_dirname + '/dist'));
+app.listen(process.env.PORT || 8080);
